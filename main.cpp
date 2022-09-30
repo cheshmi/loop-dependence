28ea0328fd6901741e8da4f84660bd70de272b29
Kazem <kazem.cheshmi@gmail.com>
2022-09-29 20:05:50 -0400

    added timer

--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 // Created by kazem on 2022-08-26.
 //
 
-#include "utils.h"
-
-
 
 #include "def.h"
 #include "sparse_blas_lib.h"
 #include "test_utils.h"
+#include "utils.h"
 #include "sparse_utilities.h"
 #include "sparse_io.h"
 #include "lbc.h"
+
+#include "utils_loop_cd.h"
 
 #include <iostream>
 
@@ -23,49 +23,88 @@
 
 #include <omp.h>
 
-void test_dense();
+#define NTIMES 5
+#define RVAL 0.5
+using namespace sym_lib;
+
+
+int test_dense(int argc, char *argv[]);
 int test_sparse(int argc, char *argv[]);
 
 
 int main(int argc, char *argv[]){
-
- test_dense();
- test_sparse(argc, argv);
+ int dense_sw = 0;
+ if(argc > 1){
+  dense_sw = atoi(argv[1]);
+ }
+ if(dense_sw){
+  std::cout<<"Dense,";
+  test_dense(argc, argv);
+ }
+ else{
+  std::cout<<"Sparse,";
+  test_sparse(argc-1, argv+1);
+ }
+ std::cout<<"\n";
  return 1;
 }
 
 
-void test_dense(){
- int m = 50;
+int test_dense(int argc, char *argv[]){
+ assert(argc>2);
+ int m = atoi(argv[2]);
  auto matrix = new double [m*m]();
  rand_test_matrix(m, matrix);
  auto vec = new double[m]();
  auto y = new double[m]();
- std::fill_n(vec, m, 0.5);
- cblas_dgemv(CblasRowMajor, CblasNoTrans,
-             m,  m,
-             1.,              // alpha
-             matrix, m,
-             vec, 1,
-             0.,              // beta
-             y, 1);
- for (int i = 0; i < m; ++i) {
-  std::cout<<y[i]<<", ";
+ auto y_cpy = new double[m]();
+ /// MV -> y = matrix * vec
+ std::vector<timing_measurement> t_mv_array;
+ for (int i = 0; i < NTIMES; ++i) {
+  std::fill_n(vec, m, RVAL);
+  timing_measurement t_mv; t_mv.start_timer();
+  cblas_dgemv(CblasRowMajor, CblasNoTrans,
+              m,  m,
+              1.,              // alpha
+              matrix, m,
+              vec, 1,
+              0.,              // beta
+              y, 1);
+  t_mv.measure_elapsed_time();
+  t_mv_array.push_back(t_mv);
  }
- std::cout<<"\n";
-
- cblas_dtrsm(CblasRowMajor, CblasRight, CblasLower, CblasConjTrans,
-             CblasNonUnit,
-             m, m, 1,
-             matrix, m, y, m);
-
- for (int i = 0; i < m; ++i) {
-  std::cout<<y[i]<<", ";
+ auto t_mv_sec = time_median(t_mv_array).elapsed_time;
+ std::copy(y, y+m, y_cpy);
+ /// TRSV -> y = matrix \ y
+ std::vector<timing_measurement> t_ts_array;
+ for (int i = 0; i < NTIMES; ++i) {
+  std::copy(y_cpy, y_cpy+m, y);
+  timing_measurement t_ts; t_ts.start_timer();
+  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans,
+              CblasNonUnit,
+              m, 1, 1,
+              matrix, m, y, 1);
+  t_ts.measure_elapsed_time();
+  t_ts_array.push_back(t_ts);
  }
- std::cout<<"\n";
+ double t_sv_sec = time_median(t_ts_array).elapsed_time;
+ /// Logging
+ std::cout<<m<<","<<m<<","<<m*m<<","<<t_mv_sec<<","<<t_sv_sec<<",";
+ /// Testing
+ for (int i = 0; i < m; ++i) {
+  if(std::abs(y[i] - RVAL) > 1e-5){
+   std::cout<<"ERROR in "<<i<< " diff "<< std::abs(y[i] - RVAL)<<",";
+   return 0;
+  }
+ }
+ std::cout<<"PASSED,";
+ delete []y;
+ delete []y_cpy;
+ delete []vec;
+ delete []matrix;
+ return 1;
 }
 
-using namespace sym_lib;
 
 int test_sparse(int argc, char *argv[]){
  CSC *L1_csc, *A = NULLPNTR;
@@ -117,16 +156,24 @@ int test_sparse(int argc, char *argv[]){
  delete[]perm;
 #endif
  L1_csr = csc_to_csr(L1_csc);
- double *y = new double[n](), *x = new double[n]();
-
+ auto *y = new double[n](), *x = new double[n]();
+ auto *y_cpy = new double[n]();
  //////// SpMV
- std::fill_n(x, n, .1);
- sym_lib::spmv_csr_parallel(n, L1_csr->p, L1_csr->i, L1_csr->x, x, y);
-
+ std::vector<timing_measurement> t_spmv_array;
+ for (int i = 0; i < NTIMES; ++i) {
+  std::fill_n(x, n, RVAL);
+  timing_measurement t_spmv; t_spmv.start_timer();
+  sym_lib::spmv_csr_parallel(n, L1_csr->p, L1_csr->i, L1_csr->x, x, y);
+  t_spmv.measure_elapsed_time();
+  t_spmv_array.push_back(t_spmv);
+ }
+ auto t_spmv_sec = time_median(t_spmv_array).elapsed_time;
+ std::copy(y, y+n, y_cpy);
  //////// SpTRSV
+ std::vector<timing_measurement> t_sptrsv_array;
  int final_level_no, *fina_level_ptr, *final_part_ptr, *final_node_ptr;
  int part_no;
- int lp_, cp_, ic_;
+ int lp_=6, cp_=4, ic_=4;
  auto *cost = new double[n]();
  for (int i = 0; i < n; ++i) {
   cost[i] = L1_csr->p[i+1] - L1_csr->p[i];
@@ -140,11 +187,27 @@ int test_sparse(int argc, char *argv[]){
                                   lp_,cp_, ic_, cost);
 
  /// SpTRSV Executor
- sptrsv_csr_lbc(n, L1_csr->p, L1_csr->i, L1_csr->x, y,
-                final_level_no, fina_level_ptr,
-                final_part_ptr, final_node_ptr);
+ for (int i = 0; i < NTIMES; ++i) {
+  std::copy(y_cpy, y_cpy+n, y);
+  timing_measurement t_sptrsv; t_sptrsv.start_timer();
+  sptrsv_csr_lbc(n, L1_csr->p, L1_csr->i, L1_csr->x, y,
+                 final_level_no, fina_level_ptr,
+                 final_part_ptr, final_node_ptr);
+  t_sptrsv.measure_elapsed_time();
+  t_sptrsv_array.push_back(t_sptrsv);
+ }
+ auto t_sptrsv_sec = time_median(t_sptrsv_array).elapsed_time;
 
- print_vec("Y: ", 0, n, y);
+ /// Logging
+ std::cout<<n<<","<<n<<","<<L1_csr->nnz<<","<<t_spmv_sec<<","<<t_sptrsv_sec<<",";
+ /// Testing
+ for (int i = 0; i < n; ++i) {
+  if(std::abs(y[i] - RVAL) > 1e-5){
+   std::cout<<"ERROR in "<<i<< " diff "<< std::abs(y[i] - RVAL)<<",";
+   return 0;
+  }
+ }
+ std::cout<<"PASSED,";
 
  delete A;
  delete L1_csc;
@@ -152,4 +215,8 @@ int test_sparse(int argc, char *argv[]){
  delete []x;
  delete []y;
  delete []cost;
+ delete []fina_level_ptr;
+ delete []final_part_ptr;
+ delete []final_node_ptr;
+ return 1;
 }
--- utils_loop_cd.h
+++ utils_loop_cd.h
@@ -2,13 +2,21 @@
 // Created by kazem on 2022-08-26.
 //
 
+
 #ifndef LOOP_DEPENDENCE_UTILS_H
 #define LOOP_DEPENDENCE_UTILS_H
 
+#include <random>
+
 void rand_test_matrix(int m, double *mat){
+ std::random_device rd;
+ std::mt19937 mt(rd());
+ std::uniform_real_distribution<double> dist(.0, 1.0);
  for (int i = 0; i < m; ++i) {
   for (int j = 0; j <= i; ++j) {
-   mat[i*m+j] = 1.0;
+   mat[i*m+j] = dist(mt);
+   if(i == j)
+    mat[i*m+j] += 10;//for numerical stability
   }
  }
 }
