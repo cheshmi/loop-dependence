3a0459402114219a9f056b82861e3a00e55c2c86
Kazem <kazem.cheshmi@gmail.com>
2022-12-24 11:57:31 -0500

    hacky setup for sparse mkl

--- run_niagara.sh
+++ run_niagara.sh
@@ -11,8 +11,8 @@
 #SBATCH --output="DDT.%j.%N.out"
 #SBATCH -t 10:00:00
 
-module load NiaEnv/.2022a
-module load intel/2022u2
+module load NiaEnv
+module load intel
 export MKL_DIR=$MKLROOT
 module load cmake
 module load gcc
@@ -23,14 +23,16 @@ which g++
 which gdb
 which make
 
+export MKLROOT=/scinet/intel/2019u4/compilers_and_libraries_2019.4.243/linux/mkl
 #### Build
 mkdir build
 cd build
 #make clean
 #rm -rf *.txt
-cmake -DCMAKE_PREFIX_PATH="$MKLROOT/lib/intel64;$MKLROOT/include;$MKLROOT/../compiler/lib/intel64;_deps/openblas-build/lib/;/home/m/mmehride/kazem/programs/metis-5.1.0/libmetis;/home/m/mmehride/kazem/programs/metis-5.1.0/include/;"  -DCMAKE_BUILD_TYPE=Release ..
-make -j 40
-
+echo " ->  $MKLROOT"
+cmake -DCMAKE_PREFIX_PATH="$MKLROOT/lib/intel64;$MKLROOT/include;$MKLROOT/../compiler/lib/intel64;_deps/openblas-build/lib/;/home/m/mmehride/kazem/programs/metis-5.1.0/libmetis;/home/m/mmehride/kazem/programs/metis-5.1.0/include/;" -DMKL_DIR=$MKLROOT -DMKL_ROOT_PATH=$MKLROOT  -DCMAKE_BUILD_TYPE=Release ..
+#make -j 40
+make sparse_mkl_test
 
 cd ..
 
@@ -47,24 +49,24 @@ export OMP_DYNAMIC=FALSE;
 
 
 mkdir logs
-echo "Config,N,M,NNZ,MV (sec),SV (sec),Cores,Test," > logs/dense.csv
-for i in {100..20000..100} ; do
-  ${BINDIR}/dense_test $i $NUM_THREAD >> logs/dense.csv
-done
-
-
-SPD_MATS=($(ls $SPD_MAT_DIR))
-echo "Config,Div,N,M,NNZ,MV (sec),SV (sec),Inspection (sec),Matrix Name,Method,Seq SV (sec),Cores,Test," > logs/sparse_lbc.csv
-for mat in "${SPD_MATS[@]}"; do
-  for div in 2 4 6 8 10 20 50 100 200; do
-      ${BINDIR}/sparse_test $SPD_MAT_DIR/$mat $NUM_THREAD 0 $div >> logs/sparse_lbc.csv
-   done
-done
-
-
-
-
-#echo "Config,N,M,NNZ,MV (sec),MV Inspection (sec),SV (sec),Inspection (sec),Matrix Name,Method,Seq SV (sec),Cores,Test," > logs/sparse_lbc.csv
-#for mat in $SPD_MAT_DIR/*.mtx; do
-#  ${BINDIR}/sparse_mkl_test ${mat} $NUM_THREAD # >> logs/sparse_mkl.csv
+#echo "Config,N,M,NNZ,MV (sec),SV (sec),Cores,Test," > logs/dense.csv
+#for i in {100..20000..100} ; do
+#  ${BINDIR}/dense_test $i $NUM_THREAD >> logs/dense.csv
 #done
+#
+#
+#SPD_MATS=($(ls $SPD_MAT_DIR))
+#echo "Config,Div,N,M,NNZ,MV (sec),SV (sec),Inspection (sec),Matrix Name,Method,Seq SV (sec),Cores,Test," > logs/sparse_lbc.csv
+#for mat in "${SPD_MATS[@]}"; do
+#  for div in 2 4 6 8 10 20 50 100 200; do
+#      ${BINDIR}/sparse_test $SPD_MAT_DIR/$mat $NUM_THREAD 0 $div >> logs/sparse_lbc.csv
+#   done
+#done
+
+
+
+
+echo "Config,N,M,NNZ,MV (sec),MV Inspection (sec),SV (sec),Inspection (sec),Matrix Name,Method,Seq SV (sec),Cores,Test," > logs/sparse_mkl.csv
+for mat in $SPD_MAT_DIR/*.mtx; do
+  ${BINDIR}/sparse_mkl_test ${mat} $NUM_THREAD  >> logs/sparse_mkl.csv
+done
